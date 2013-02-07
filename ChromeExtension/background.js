497a14067c81b3331e1f13788730223306ef054c
Botao Hu <me@botao.hu>
2013-02-07 05:33:32 -0800

    asdasd

--- ChromeExtension/background.js
+++
@@ -1,4 +0,0 @@
-var doc = chrome.extension.getBackgroundPage().document;
-var setup = doc.createElement('script');
-setup.src = "setup.js";
-doc.head.appendChild(setup);
--- ChromeExtension/extension.js
+++
@@ -1,7 +0,0 @@
-//var server = "https://mailfoogae.appspot.com";
-
-//var server = "https://lime-time.appspot.com";
-var server = "https://localhost";
-
-var extVersion = "5.0";
-var combinedPath = "/build/";
